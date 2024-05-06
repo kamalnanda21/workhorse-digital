b5e6ee1a9eafe53a1403e85f18de635939f2fb10
kamalnanda21 <kamalnanda01@gmail.com>
2024-05-07 00:55:14 +0530

    mobile navbar fixes

--- scripts/main.js
+++ scripts/main.js
@@ -56,6 +56,11 @@ const mobileNavbar = document.getElementById("mobile-navbar");
 hamburger.addEventListener("click", function() {
   mobileNavbar.classList.toggle("open");
   hamburger.classList.toggle("cross");
+  if (mobileNavbar.classList.contains("open")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
   $('.submenu').hide();
 });
 
